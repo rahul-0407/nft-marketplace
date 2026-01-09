385de5ee897ad090ca84f4189c79db7f0a2cbd93
rahul-0407 <rahulkrgupta0407@gmail.com>
2026-01-10 03:18:29 +0530

    workig on discover

--- nftfrontend/components/NavBar/Discover.jsx
+++ nftfrontend/components/NavBar/Discover.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
 
 const Discover = () => {
+
+  const discover = [
+    {
+      name: "Collection",
+      link: "collection"
+    },
+    {
+      name: "Searc",
+      link: "search"
+    },
+    {
+      name: "Author Profile",
+      link: "author-profile"
+    },
+    {
+      name: "NFT Details",
+      link: "NFT-details"
+    },
+    {
+      name: "Account Setting",
+      link: "account-setting"
+    },
+    {
+      name: "Connect Wallet",
+      link: "connect-wallet"
+    },
+    {
+      name: "Blog",
+      link: "blog"
+    },
+
+  ]
+
+
   return (
-    <div>Discover</div>
+    <div>
+      {discover.map((el,i)=>(
+        <div key={i+1} className='discover'>
+          <Link href={{pathname: `${el.link}`}}>
+          {el.name}
+          </Link>
+        </div>
+      ))}
+    </div>
   )
 }
 
