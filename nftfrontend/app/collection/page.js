656921ed7b1fd95630bd7c4391878123cad948e1
rahul-0407 <rahulkrgupta0407@gmail.com>
2026-01-24 23:57:36 +0530

    working on nftCardTwo

--- nftfrontend/app/collection/page.js
+++ nftfrontend/app/collection/page.js
@@ -1,19 +1,35 @@
-import React from 'react'
-import Banner from '@/components/Banner'
-import CollectionProfile from '@/components/CollectionProfile'
-import Filter from '@/components/Filter'
-// import Slider from '@/components/Slider'
-// import Brand from '@/components/Brand'
+import React from "react";
+import Banner from "@/components/Banner";
+import CollectionProfile from "@/components/CollectionProfile";
+import Filter from "@/components/Filter";
+import Slider from '@/components/Slider'
+import Brand from '@/components/Brand'
+import NFTCardTwo from "@/components/NFTCardTwo";
 // import Filter from '@/components/Filter'
 
 const Collection = () => {
-  return (
-    <div className='collection'>
-        <Banner bannerImage="./creatorbackground-1.jpeg" />
-        <CollectionProfile/>
-        <Filter/>
-    </div>
-  )
-}
+  const collectionArray = [
+    "./nft-image-1.png",
+    "./nft-image-2.png",
+    "./nft-image-3.png",
+    "./nft-image-1.png",
+    "./nft-image-2.png",
+    "./nft-image-3.png",
+    "./nft-image-1.png",
+    "./nft-image-2.png",
+    "./nft-image-3.png",
+  ];
 
-export default Collection
+  return (
+    <div className="collection">
+      <Banner bannerImage="./creatorbackground-1.jpeg" />
+      <CollectionProfile />
+      <NFTCardTwo NFTData={collectionArray}/>
+      <Filter />
+      <Slider/>
+      <Brand/>
+    </div>
+  );
+};
+
+export default Collection;
--- nftfrontend/components/NFTCardTwo.jsx
+++ nftfrontend/components/NFTCardTwo.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+'use client'
 
-const NFTCardTwo = () => {
+import React,{useState} from 'react'
+import { BsImage } from "react-icons/bs";
+import {AiFillHeart, AiOutlineHeart} from "react-icons/ai"
+import {MdTimer, MdVerified} from "react-icons/md"
+import LikeProfile from './LikeProfile';
+
+const NFTCardTwo = ({NFTData}) => {
+
+    const [like, setLike] = useState(false)
+    const [likeInc, setLikeInc] = useState(21)
+
+    const likeNft = () => {
+        if(!like){
+            setLike(true)
+            setLikeInc(23)
+        } else {
+            setLike(false)
+            setLikeInc(23+1)
+        }
+    }
+
   return (
-    <div>NFTCardTwo</div>
+    <div className='NFTCardTwo'>
+        {NFTData.map((el, i)=>(
+            <div className="NFTCardTwo_box" key={i+1}>
+                <div className="NFTCardTwo_box_like">
+                    <div className="NFTCardTwo_box_like_box">
+                        <div className="NFTCardTwo_box_like_box_box">
+                            <BsImage className='NFTCardTwo_box_like_box_box_icon' />
+                            <p onClick={()=>likeNft()}>
+                                {like? <AiOutlineHeart/>:<AiFillHeart/>}
+                                {""}
+                                <span>{likeInc+1}</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="NFTCardTwo_box_img">
+                    <img src={el} alt="NFT" height={500} width={500} objectFit="cover" className='NFTCardTwo_box_img_img'/>
+                </div>
+                <div className="NFTCardTwo_box_info">
+                    <div className="NFTCardTwo_box_info_left">
+                        <LikeProfile/>
+                        <p>Clone {i+1}</p>
+                    </div>
+                    <small>4{i+2}</small>
+                </div>
+                <div className="NFTCardTwo_box_price">
+                    <div className="NFTCardTwo_box_price_box">
+                        <small>Current Bids</small>
+                        <p>1{i+5}</p>
+                    </div>
+                    <p className='NFTCardTwo_box_price_stock'><MdTimer/> <span>{i+1} hours left</span></p>
+                </div>
+            </div>
+        ))}
+    </div>
   )
 }
 
