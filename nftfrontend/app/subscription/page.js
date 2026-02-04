035c53643b8b3e083dac7c848472f52a3c5c621d
rahul-0407 <rahulkrgupta0407@gmail.com>
2026-02-04 05:59:10 +0530

    fixing layout

--- nftfrontend/app/subscription/page.js
+++ nftfrontend/app/subscription/page.js
@@ -1,19 +1,20 @@
+
 import React from "react";
-import SubscriptionBox from "@/components/SubscriptionBox";
+import SubscriptionBox from "@/components/Subscription";
 
 const Subscription = () => {
   const subscriptionArray = [
     {
       plan: "STARTER",
       price: "$5/mo",
-      popular: "",
+      popular: " ",
       service: ["Automated Reporting", "Faster Processing", "Customizations"],
       info: "Literally you probably haven't heard of them jean shorts,",
     },
     {
       plan: "BASIC",
       price: "$15/mo",
-      popular: "",
+      popular: "POPULAR",
       service: [
         "Everyting in Starter",
         "100 Builds",
@@ -25,7 +26,7 @@ const Subscription = () => {
     {
       plan: "PLUS",
       price: "$25/mo",
-      popular: "",
+      popular: " ",
       service: [
         "Everyting in Basic",
         "Unlimited Builds",
@@ -37,15 +38,15 @@ const Subscription = () => {
   ];
 
   return (
-    <div className="w-full my-28 max-[35em]:my-16 max-[1024px]:my-16">
+    <div className="w-full my-28 max-[35em]:my-16 max-[1024px]:my-16 text-(--icons-color)">
       <div className="w-[80%] mx-auto max-[35em]:w-[90%] max-[1024px]:w-[90%]">
 
         {/* HEADER */}
-        <div className="text-center leading-none">
-          <h1 className="text-[4rem] leading-[0.5] max-[35em]:text-[2rem] max-[1024px]:text-[2rem]">
+        <div className="text-center ">
+          <h1 className="text-[4rem]  max-[35em]:text-[2rem] max-[1024px]:text-[2rem] font-semibold">
             💎 Subscription
           </h1>
-          <p className="text-[1.6rem] leading-[0.5] max-[35em]:text-[1rem] max-[1024px]:text-[1rem]">
+          <p className="text-[1.6rem]  max-[35em]:text-[1rem] max-[1024px]:text-[1rem]">
             Pricing to fit the needs of any company size.
           </p>
         </div>
--- nftfrontend/components/Subscription.jsx
+++ nftfrontend/components/Subscription.jsx
@@ -1,29 +1,29 @@
+'use client'
+
 import React from "react";
 import { TiTick } from "react-icons/ti";
 import Button from "./Button";
 
-const SubscriptionBox = ({ el, i }) => {
+const SubscriptionBox = ({ el }) => {
   return (
     <div
       className="
-        border border-[var(--icons-color)]
+        border border-(--icons-color)
         p-8 rounded-xl
         relative cursor-pointer
         transition-all duration-300 ease-in
-        hover:shadow-[var(--box-shadow)]
+        hover:shadow-(--box-shadow)
       "
     >
-      <span className="text-[2rem] font-black">
-        {el.plan}
-      </span>
+      <span className="text-[2rem] font-black">{el.plan}</span>
 
       {el.popular && (
         <small
           className="
             absolute right-16
             text-[1.2rem]
-            bg-[var(--icons-color)]
-            text-[var(--main-bg-color)]
+            bg-(--icons-color)
+            text-(--main-bg-color)
             px-4 py-2 rounded-lg
           "
         >
@@ -31,9 +31,7 @@ const SubscriptionBox = ({ el, i }) => {
         </small>
       )}
 
-      <p className="text-[3rem] leading-none font-black">
-        {el.price}
-      </p>
+      <p className="text-[3rem]  font-black">{el.price}</p>
 
       <div className="my-8">
         {el.service.map((service, i) => (
@@ -41,7 +39,7 @@ const SubscriptionBox = ({ el, i }) => {
             key={i}
             className="
               flex items-center gap-2
-              font-medium text-[1.5rem] leading-none
+              font-medium text-[1.5rem]
               max-[35em]:text-[1rem]
             "
           >
@@ -53,11 +51,7 @@ const SubscriptionBox = ({ el, i }) => {
         ))}
       </div>
 
-      <Button
-        btnName="Submit"
-        handleClick={() => {}}
-        classStyle="mt-8"
-      />
+      <Button btnName="Submit" handleClick={() => {}} classStyle="mt-8" />
     </div>
   );
 };
